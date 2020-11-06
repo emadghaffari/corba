5d33fed33b2566d70aa9da522f2fcaa568d87bfb
Emad Ghaffari <emadghaffariii@gmail.com>
2020-11-06 11:11:20 +0000

    generate random hash with spesific len

---
+++ cmd/hash.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	lenght  int
+	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789-@%$&()_")
+)
+
+func init() {
+	rootCmd.AddCommand(hashCmd)
+	hashCmd.Flags().IntVarP(&lenght, "lenght", "l", 1, "lenght for random hash string")
+	hashCmd.MarkFlagRequired("lenght")
+}
+
+var hashCmd = &cobra.Command{
+	Use:   "hash",
+	Short: "generate a string of a fixed length",
+	Run: func(cmd *cobra.Command, args []string) {
+		rand.Seed(time.Now().UnixNano())
+		b := make([]rune, lenght)
+		for i := range b {
+			b[i] = letters[rand.Intn(len(letters))]
+		}
+
+		fmt.Printf("%s \n", string(b))
+	},
+}
